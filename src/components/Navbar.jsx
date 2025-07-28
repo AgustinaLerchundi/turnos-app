a00b6ec4bad2367fead5aca1bb3c1e0cb5d70567
agustina lerchundi <aguslerchundii@gmail.com>
2025-07-28 10:42:09 -0300

    git commit -m "commit"
    
    
    i
    fix: ajustado el ancho de Home y estilos del Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import styles from './Navbar.module.css';
+import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.navbarContent}>
-                <h1 className={styles.logo}>Gestor turnos</h1>
-                <ul className={styles.navLinks}>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Servicios</a></li>
-                    <li><a href="#">Contacto</a></li>
-                </ul>
-            </div>
-</nav>
-      
-    )
+  return (
+    <nav className="navbar">
+      <h1>TurnosApp</h1>
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Profesionales</a></li>
+        <li><a href="#">Reservar</a></li>
+        <li><a href="#">Ingresar</a></li>
+      </ul>
+    </nav>
+  );
 };
 
-
-
 export default Navbar;
+
+
+
+
